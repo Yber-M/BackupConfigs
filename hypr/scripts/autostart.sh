02984dcc6a92f100f5a6162c8cd4ccb99fac9a7b
Yber-M <ybermarlon@gmail.com>
2026-05-15 17:37:30 -0500

    Auto backup 2026-05-15 17:37

--- hypr/scripts/autostart.sh
+++ hypr/scripts/autostart.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 
-# HyDE/Waybar estable
-sleep 3
+# Waybar/HyDE estable
+sleep 2
 
 if ! pgrep -x waybar >/dev/null; then
     hyde-shell reload &
 fi
 
-# Apps después
-sleep 5
+# Apps
+sleep 2
 
 pgrep -x copyq >/dev/null || copyq &
-sleep 1
-
 pgrep -f "edge-whatsapp" >/dev/null || gtk-launch whatsapp &
-sleep 1
-
 pgrep -f "edge-whatsapp-business" >/dev/null || gtk-launch whatsapp-business &
-sleep 1
-
 pgrep -x vesktop >/dev/null || vesktop &
-sleep 1
-
 pgrep -x Cider >/dev/null || cider &
