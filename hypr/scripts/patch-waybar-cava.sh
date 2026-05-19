106d2ab9969ceecacc7ec548cc5172f7adda3ede
Yber-M <ybermarlon@gmail.com>
2026-05-19 00:29:27 -0500

    Auto backup 2026-05-19 00:29

--- hypr/scripts/patch-waybar-cava.sh
+++ hypr/scripts/patch-waybar-cava.sh
@@ -2,11 +2,7 @@
 
 CONFIG="$HOME/.config/waybar/config.jsonc"
 
-# Esperar a que HyDE regenere config
 sleep 3
-
-# Si ya existe custom/cava no hacer nada
-grep -q '"custom/cava"' "$CONFIG" && exit 0
 
 python - <<'PY'
 from pathlib import Path
@@ -14,18 +10,19 @@ from pathlib import Path
 p = Path.home() / ".config/waybar/config.jsonc"
 s = p.read_text()
 
-# Insertar módulo en center
-s = s.replace(
+# Center: agregar cava
+if '"custom/cava"' not in s:
+    s = s.replace(
 '''"hyprland/workspaces",
             "hyprland/window"''',
 '''"hyprland/workspaces",
             "custom/cava",
             "hyprland/window"'''
-)
+    )
 
-# Insertar definición del módulo
-insert = '''
-    "custom/cava": {
+    s = s.replace(
+'''    "modules-right": [''',
+'''    "custom/cava": {
         "exec": "/home/yb-m/.config/waybar/scripts/cava.sh",
         "format": "{}",
         "return-type": "json",
@@ -33,13 +30,43 @@ insert = '''
         "restart-interval": 1,
         "on-click": "/home/yb-m/.config/eww/scripts/toggle-media.sh"
     },
-'''
-
-s = s.replace(
-'''    "modules-right": [''',
-insert + '''
     "modules-right": ['''
+    )
+
+# Left: reemplazar cpu/memory por custom
+s = s.replace(
+'''        "modules": [
+            "cpu",
+            "memory"
+        ]''',
+'''        "modules": [
+            "custom/cpu",
+            "custom/ram",
+            "custom/disk"
+        ]'''
 )
+
+# Insertar módulos custom CPU/RAM/DISK
+if '"custom/cpu": {' not in s:
+    s = s.replace(
+'''    "modules-left": [''',
+'''    "custom/cpu": {
+        "exec": "top -bn1 | grep 'Cpu(s)' | awk '{print int($2)}'",
+        "interval": 2,
+        "format": "󰍛 {}%"
+    },
+    "custom/ram": {
+        "exec": "free -h | awk '/Mem:/ {print $3}'",
+        "interval": 3,
+        "format": "󰘚 {}"
+    },
+    "custom/disk": {
+        "exec": "df -h / | awk 'NR==2 {print $3\\"/\\"$2}'",
+        "interval": 30,
+        "format": "󰋊 {}"
+    },
+    "modules-left": ['''
+    )
 
 p.write_text(s)
 PY
