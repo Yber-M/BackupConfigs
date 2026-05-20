fb57c454a8a9e7839e0070afb2aacdd6649c6d22
Yber-M <ybermarlon@gmail.com>
2026-05-19 23:29:18 -0500

    Auto backup 2026-05-19 23:29

--- hypr/scripts/toggle-gamemode.sh
+++ hypr/scripts/toggle-gamemode.sh
@@ -3,15 +3,21 @@
 STATE="/tmp/hypr-gamemode"
 
 if [ -f "$STATE" ]; then
+    # OFF → volver normal
     hyprctl keyword animations:enabled 1
     hyprctl keyword decoration:blur:enabled true
-    notify-send "GameMode OFF"
+    hyprctl keyword decoration:active_opacity 0.95
+    hyprctl keyword decoration:inactive_opacity 0.90
 
+    notify-send "GameMode OFF"
     rm "$STATE"
 else
+    # ON → sólido + rendimiento
     hyprctl keyword animations:enabled 0
     hyprctl keyword decoration:blur:enabled false
-    notify-send "GameMode ON"
+    hyprctl keyword decoration:active_opacity 1.0
+    hyprctl keyword decoration:inactive_opacity 1.0
 
+    notify-send "GameMode ON"
     touch "$STATE"
 fi
