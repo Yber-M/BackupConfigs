e0541306191d932e28544c982a5158dc1bf60d77
Yber-M <ybermarlon@gmail.com>
2026-07-21 23:42:11 -0500

    Auto backup 2026-07-21 23:42

--- hypr/scripts/autostart.sh
+++ hypr/scripts/autostart.sh
@@ -7,12 +7,16 @@ flock -n 9 || exit 0
 
 sleep 7
 
-~/.config/hypr/scripts/patch-waybar-cava.sh
+#~/.config/hypr/scripts/patch-waybar-cava.sh
 
-killall -SIGUSR2 waybar 2>/dev/null
+#killall -SIGUSR2 waybar 2>/dev/null
 
 sleep 2
 pgrep -x copyq >/dev/null || copyq &
 pgrep -x ferdium >/dev/null || ferdium &
 pgrep -fi "discord" >/dev/null || discord &
 pgrep -x cider >/dev/null || cider &
+
+pgrep -x awww-daemon >/dev/null || awww-daemon &
+sleep 1
+pgrep -f "ags run" >/dev/null || ags run &
