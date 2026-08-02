bba7e9809f1b6eff86129e0d60ba7d7f0207b8e3
Yber-M <ybermarlon@gmail.com>
2026-08-02 14:48:18 -0500

    Auto backup 2026-08-02 14:48

--- local-bin/cider-notify.sh
+++ local-bin/cider-notify.sh
@@ -2,17 +2,18 @@
 CACHE_DIR="$HOME/.cache/cider-notify"
 mkdir -p "$CACHE_DIR"
 
-playerctl -p cider --follow metadata --format '{{title}}|||{{artist}}|||{{mpris:artUrl}}' 2>/dev/null | while IFS='|||' read -r title artist arturl; do
+playerctl -p cider --follow metadata --format $'{{title}}\t{{artist}}\t{{mpris:artUrl}}' 2>/dev/null | \
+while IFS=$'\t' read -r title artist arturl; do
   [ -z "$title" ] && continue
 
   img_path="$CACHE_DIR/current.jpg"
   if [ -n "$arturl" ]; then
-    curl -s -o "$img_path" "$arturl" 2>/dev/null
+    curl -sL -o "$img_path" "$arturl" 2>/dev/null
   fi
 
   if [ -s "$img_path" ]; then
-    notify-send -a "Cider" -i "$img_path" "$title" "$artist"
+    notify-send -a "Apple Music" -i "$img_path" "$title" "$artist"
   else
-    notify-send -a "Cider" -i "audio-x-generic" "$title" "$artist"
+    notify-send -a "Apple Music" -i "audio-x-generic" "$title" "$artist"
   fi
 done
